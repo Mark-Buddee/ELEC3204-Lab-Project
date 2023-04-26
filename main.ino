410622009d12ab9abb93a47bd1b7932656842f00
Mark-Buddee <104953342+Mark-Buddee@users.noreply.github.com>
2023-04-26 16:02:45 +1000

    Update main.ino
    Got rid of the bullshit, added good direction shit. Only measures frequency from one of the out pins of the motor

--- main.ino
+++ main.ino
@@ -4,17 +4,14 @@
 
 #define PWMA 9
 #define PWMB 10
-#define INPIN1 1
-#define INPIN2 2
+#define INPIN1 2
+#define INPIN2 4
 
-const int onTime = 127;
-volatile int t1 = 0;
-volatile int T1;
-volatile int dt1;
+volatile int t = 0;
+volatile int T;
+volatile int dt;
 
-volatile int t2 = 0;
-volatile int T2;
-volatile int dt2;
+volatile int direction;
 
 //------------------------- setup routine ----------------------------//
 void setup() {
@@ -34,22 +31,23 @@ void setup() {
   TCCR1B |= _BV(CS11); //set prescaler=8 by lettin the bit value of CS11=1 in register TCCR1B, so the clock frequency=16MHz/8=2MHz
   ICR1 = 100;//  phase correct PWM. PWM frequency determined by counting up 0-100 and counting down 100-0 in the input compare register (ICR1), so freq=200*0.5us=10kHz 
 
-  attachInterrupt(digitalPinToInterrupt(INPIN1),signalRise1,RISING);
-  attachInterrupt(digitalPinToInterrupt(INPIN2),signalRise2,RISING);
+  attachInterrupt(digitalPinToInterrupt(INPIN1),edgeRise,RISING);
 }
 
 //------------------------- main loop ----------------------------//
 void loop() 
 {
   PWM(30); //set duty of PWM as 30%
-  delay(1000);
 
-  float f1 = 1000000/dt1;
-  Serial.print("f1 = ");
-  Serial.println(f1);
-  float f2 = 1000000/dt2;
-  Serial.print("f2 = ");
-  Serial.println(f2);
+  // Print the direction to the serial monitor
+  Serial.print("  direction = ");
+  Serial.print(direction);
+
+  float f = 1000000/dt;
+  Serial.print("  f = ");
+  Serial.println(f);
+
+  // delay(1000);
 }
 
 //------------------------- subroutine PWM generate complementary PWM from OCR1A and OCR1B ----------------------------//
@@ -69,15 +67,16 @@ void PWM(int pwm)
 }
 
 
-void signalRise1() {                    
-  T1 = micros();
-  dt1 = T1 - t1;
-  t1 = micros();
-}
-
-
-void signalRise2() {                    
-  T2 = micros();
-  dt2 = T2 - t2;
-  t2 = micros();
+void edgeRise() {
+  if(digitalRead(INPIN2) == LOW) {
+    direction = 1;
+  } else if(digitalRead(INPIN2) == HIGH) {
+    direction = -1;
+  } else {
+    Serial.println("error");
+  }
+  
+  T = micros();
+  dt = T - t;
+  t = micros();
 }
